395628f74d488623e5f07a9d79758ed66c242178
W.P <william.pourmajidi@gmail.com>
2024-05-14 11:34:22 -0400

    Adding Global Variable Example

---
+++ Lecture-2.py
@@ -0,0 +1,14 @@
+# How to define and modify a global variable.
+
+
+global_counter = 0
+print(f"Value of global_counter before modification: {global_counter}")
+
+def increment():
+    global global_counter
+    global_counter += 1
+
+    print(f"Value of global_counter after modification: {global_counter}")
+
+increment() # calling the modification function
+
