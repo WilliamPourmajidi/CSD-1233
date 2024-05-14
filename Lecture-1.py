da42a0581eb2b528b31ff17e605afac86bc88e08
William Pourmajidi <William.Pourmajidi@gmail.com>
2024-05-14 07:10:39 -0400

    Initial commit

---
+++ Lecture-1.py
@@ -0,0 +1,60 @@
+# Defining Variables
+
+name = "William"  # Variable holding a string
+age = 42  # Variable holding an integer
+pi = 3.14  # Variable holding a float (decimal values or fractional numbers like 42.5 or 13.2)
+
+# Arithmetic Operation
+
+a = 10
+b = 5
+p = 3.14
+# Adding two integers
+sum = a + b  # Addition
+diff = a - b  # Subtraction
+print(sum)
+print(diff)
+
+# Adding a float to an integer
+
+b = 5
+f = 3.14
+
+c = f + b
+print(c)
+# Trick question: What is the type of c ?
+
+# Calculating the area of a Circle
+
+
+PI = 3.14159
+radius = 7
+area = PI * radius ** 2
+
+print("The area of the circle is", area)
+
+# Basic Solution to Homework
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum = num1 + num2
+print(sum)
+
+dif = num1 - num2
+print(dif)
+
+product = num1 * num2
+print(product)
+
+Quotient = num1 / num2
+print(Quotient)
+
+# A bit more advanced Solution to Homework
+print(f"Sum: {num1 + num2}")
+
+print(f"Difference: {num1 - num2}")
+
+print(f"Product: {num1 * num2}")
+
+print(f"Quotient: {num1 / num2}")
