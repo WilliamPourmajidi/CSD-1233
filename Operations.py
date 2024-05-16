d42c75dd40ea2724b6e3c6fadf75f2baa748553f
William Pourmajidi <William.Pourmajidi@gmail.com>
2024-05-16 10:10:04 -0400

    Adding Operations.py

---
+++ Operations.py
@@ -0,0 +1,41 @@
+# a = 10
+# b = 20
+#
+# print("Multiplication",  a * b)
+# print("Sum", a + b)
+# print(f"sum {a + b}")
+#
+# c = 13
+# d = 4
+#
+# print(c/d)  #division
+# print(c//d)  #floor division
+#
+# # 2 to the power of 3
+# print(2 ** 3)
+#
+# s = "William"
+# t = "likes"
+# u = "Python"
+#
+# print (s*t)
+# print(s +" " + t + " "+ u)
+
+##### How to find the type of a variable
+var1 = 12
+var5 = str(var1)
+print(type(var5))
+# var2 = 12.2
+# var3 = "William"
+# print(type(var1))
+# print(type(var2))
+# print(type(var3))
+# # ===========
+# b = 5
+# f = 3.14
+# c = b + f
+# print(c)
+# print(type(c))
+#
+
+
