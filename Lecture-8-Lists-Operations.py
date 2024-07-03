a6d63ca822d0357393c550d2ee15268a8a93702b
W.P <william.pourmajidi@gmail.com>
2024-07-03 09:29:54 -0400

    Adding Lecture-8 Contents

---
+++ Lecture-8-Lists-Operations.py
@@ -0,0 +1,10 @@
+# ' Create a list with different data types
+my_list = [1, 2, 3, 4, "William", 3.14]
+print(my_list)
+
+# ' Append an element to the list
+my_list.append("Awesome!")
+print(my_list)
+
+# ' Access the first element of the list
+print(my_list[0])  # Output: 1
